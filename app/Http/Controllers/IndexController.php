599c1e1d03dcd08f5b7598c2250f662a9d304694
wardaturrahmah <wardahamaliyah@gmail.com>
2021-05-30 12:24:27 +0700

    save

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -68,30 +68,116 @@ class IndexController extends Controller
     //
     public function save($input)
     {
-    	$data_hdr=$input["data_hdr"];
-    	//print_r($data_hdr);
-    	$data_dtl= $input["data_dtl"];
-    	//print_r($data_dtl);
-    	foreach($data_dtl as $dtl)
-    	{
-    		foreach ($dtl as $key => $value) {
-    			# code...
-    			//echo $key;
-    		}
-    	}
     	/*
+    	{
+		    "action" : "save",
+		    "table_hdr" : "tx_hdr_billing",
+		    "data_hdr" : [
+		        {
+		            "billing_name" : "wardah",
+		            "billing_email" :"wardah@gmail.com",
+		            "billing_phone" : "08573031111",
+		            "billing_information" : "info",
+		            "billing_address" : "SBY"
+		        }
+		    ],
+		    "table_dtl" : "tx_dtl_billing",
+		    "join_column_hdr" : "billing_id",
+		    "join_column_dtl" :"dtl_hdr_id",
+		    "data_dtl" : [
+		        {
+		        "dtl_product" :"Sabun",
+		        "dtl_qty" : 1,
+		        "dtl_price" : "2000"
+		        },
+		        {
+		        "dtl_product" :"Shampo",
+		        "dtl_qty" : 1,
+		        "dtl_price" : "500"
+		        }
+		    ]
+		}
+    	*/
     	$table_hdr = $input["table_hdr"];
+    	$table_dtl = $input["table_dtl"];
 		$insert = \DB::table($table_hdr);
 		$insert=$insert->insert($input["data_hdr"]);
-
+		$list = \DB::table($table_hdr);
+		
+		if(!empty($table_dtl))
+		{
+			$data_dtl= $input["data_dtl"];
+			$dtl_hdr_id = DB::table($table_hdr)->max($input["join_column_hdr"]);	
+	    	$in_dtl=[];
+	    	foreach($data_dtl as $dtl)
+	    	{
+				$dtl_hdr_id = DB::table($table_hdr)->max($input["join_column_hdr"]);
+	    		$temp_in[$input["join_column_dtl"]]= $dtl_hdr_id;
+	    		foreach ($dtl as $key => $value) {
+	    			$temp_in[$key]=$value;
+	    		}
+	    		array_push($in_dtl, $temp_in);
+				
+	    	}
+	    	$insert = \DB::table($table_dtl);
+			$insert=$insert->insert($in_dtl);
+			$list->join($table_dtl,$input["join_column_hdr"],"=",$input[join_column_dtl]);
+    	}
+    	
+    	$list=$list->get();
 		if($insert)
 		{
-			return "Insert Success";
+			$data=["success"=>true,"message" => "Insert Success","list"=>$list];
 		}
 		else
 		{
-			return "Insert Failure";
+			$data=["success"=>false,"message" => "Insert Failed","list"=>$list];
+		}
+		return $data;
+    }
+    public function delete($input)
+    {
+    	/*
+    	{
+		    "action": "delete",
+		    "table": "tx_product",
+		    "where" :[
+		        [
+		            "product_id","=",2
+		        ]
+		    ]
 		}
 		*/
+    	$table = $input["table"];
+		$data = \DB::table($table);
+    	if(!empty($input["where"]))
+			$data->where($input["where"]);
+		$data=$data->delete();
+		$list = \DB::table($table)->get();
+		if($data)
+		{
+			$result=["success"=>true,"message" => "Delete Success","list"=>$list];
+		}
+		else
+		{
+			$result=["success"=>false,"message" => "Delete Failed","list"=>$list];
+		}
+		return $result;
     }
+    public function upload($input)
+    {
+    	/*
+    	{
+		   action:"upload",
+		   image:"bsdafksafei9234ksafd"
+		}
+		*/
+		//$result=$this->request->file('img')->store('apidocs');
+		$folderPath = "/"; //letak foldernya kalo images/ berarti di folder public harus buat folder images 
+		$image_base64 = base64_decode($input["image"]); //ini ngedecode base64 menjadi image lagi
+		$file = $folderPath .$input["name"]. '.jpg'; //ini nama filenya ketika nanti disimpan
+		file_put_contents($file, $image_base64); //menyimpan file image kedalam folder
+		return $input;
+    }
+
 }
